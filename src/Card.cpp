ab026d433448c2e303239b5c6683238ac4de51b0
Andréa Margot <amargot850@gmail.com>
2025-12-27 22:38:24 +0100

    maj cpp

---
+++ src/Card.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "Card.hpp"
+#include <string>
+
+
+void Card::affiche()const{
+    if(spec == "p"){
+        std::cout << "[" << color << ", passe ton tour ]" << std::endl;
+    }
+    else{                    
+        std::cout << "[" << color << ", " << spec  << "]" << std::endl; 
+    }
+    };
+
+std::string Card::numero()const{
+    return spec;
+};
+
+std::string Card::numero(){
+    return spec;
+}
+
+std::string Card::colorcard()const{
+    return color;
+};
+
+std::string Card::colorcard(){
+    return color;
+}
+
+bool Card::superpose(const Card& C2)const{
+    std::string numC2=C2.numero();
+    std::string colC2=C2.colorcard();
+    return (spec == numC2) || (color==C2.color) || (color == "N") || (C2.colorcard() == "N") ;
+};
+
+bool Card::operator==(const Card& C)const{
+    return (color == C.colorcard()) && (spec == C.numero());
+};
+
+Card& Card::operator=(const Card& card){
+    if(this == &card){
+        return *this;
+    }
+    spec = card.numero();
+    color = card.colorcard();
+    return *this;
+}
---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include "Card.hpp"
+#include "Joueur.hpp"
+#include "Pioche.hpp"
+#include "Jeu.hpp"
+#include <iostream>
+#include <list>
+#include <ctime>
+#include <random>
+#include <chrono>
+
+int main(){
+    Jeu jeu;
+    //début de la partie:
+    jeu.Partie();
+    return 0;
+};
